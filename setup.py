eb99ecccbf6b375092a7a24fd6d7d484724f1240
tomcatling <tomcatling@gmail.com>
2019-03-01 11:28:53 +0000

    packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'jupyter_aws_spawner',
+    version = '1.0.0',
+    url = 'https://github.com/tomcatling/jupyter-aws-spawner',
+    author = 'Author Name',
+    author_email = 'tomcatling@gmail.com',
+    description = '-',
+    packages = find_packages(),    
+    install_requires = [],
+)
